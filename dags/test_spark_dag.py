f0be7aa2fba17b373a571a7919ac88d8c2c35769
Alex <AlexeiNumberOne@yandex.ru>
2026-02-02 13:37:04 +0300

    тест

--- dags/test_spark_dag.py
+++ dags/test_spark_dag.py
@@ -1,38 +1,36 @@
-from datetime import datetime, timedelta
-from airflow import DAG
-from airflow.operators.bash import BashOperator
+from datetime import datetime
+from airflow.decorators import dag
 
-# Аргументы по умолчанию для DAG
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2024, 1, 1),
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-}
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import (
+    SparkKubernetesOperator,
+)
+from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import (
+    SparkKubernetesSensor,
+)
 
-# Определение DAG
-with DAG(
-    dag_id='test_spark_kubernetes',
-    default_args=default_args,
-    description='Тестовый DAG для запуска Spark',
-    schedule_interval=None,  # Запуск только вручную
-    catchup=False,
-    tags=['test', 'spark'],
-) as dag:
-    
-    run_spark = BashOperator(
-        task_id='spark_bash_exec',
-        bash_command="""
-        kubectl exec -n spark deploy/spark-client-service -- \
-        /opt/spark/bin/spark-submit \
-            --master spark://spark-master:7077 \
-            /opt/spark-jobs/test_spark_job.py \
-            --date {{ ds }}
-        """
+
+@dag(
+    schedule=None,
+    start_date= datetime(2024, 3, 1),
+)
+def spark_operator():
+
+
+    submit = SparkKubernetesOperator(
+        task_id="submit",
+        namespace="airflow",
+        application_file="spark-application.yaml",
+        do_xcom_push=True,
+        params={"app_name": "spark-pi"},
     )
 
+    submit_sensor = SparkKubernetesSensor(
+        task_id="submit_sensor",
+        namespace="airflow",
+        application_name="{{ task_instance.xcom_pull(task_ids='submit')['metadata']['name'] }}",
+        attach_log=True,
+    )
 
+    submit >> submit_sensor
 
+spark_operator()
